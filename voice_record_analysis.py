01a4e4638d9a2dd15cd1f5b3ad988ff3d724e97d
Alex Brockman <brockly2@gmail.com>
2019-11-05 18:43:41 -0500

    Add voice_analysis_record.py

--- voice_record_analysis.py
+++ voice_record_analysis.py
@@ -1,20 +1,20 @@
-import argparse
+#!/usr/bin/env python3
+
 import time
 import threading
 import pyloudness
 
 from aiy.board import Board
 from aiy.voice.audio import AudioFormat, play_wav, record_file, Recorder
+from aiy.voice import tts
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--filename', '-f', default='recording.wav')
-    args = parser.parse_args()
+FILENAME = 'recording.wav'
 
+WORKTIME = 2.0
+BREAKTIME = 0.5
+
+def pomodoro():
     with Board() as board:
-        print('Press button to start recording.')
-        board.button.wait_for_press()
-
         done = threading.Event()
         board.button.when_pressed = done.set
 
@@ -22,16 +22,22 @@ def main():
             start = time.monotonic()
             while not done.is_set():
                 duration = time.monotonic() - start
-                print('Recording: %.02f seconds [Press button to stop]' % duration)
+                if duration > WORKTIME*60:
+                    break
+                print('Working: %.02f second(s) left [Press button to stop]' % (WORKTIME*60 - duration), end='\r', flush=True)
                 time.sleep(0.5)
 
-        record_file(AudioFormat.CD, filename=args.filename, wait=wait, filetype='wav')
-        print('Press button to save the recorded sound.')
-        board.button.wait_for_press()
+        record_file(AudioFormat.CD, filename=FILENAME, wait=wait, filetype='wav')
+                
+        loudness_stats = pyloudness.get_loudness(FILENAME)
         
-        loudness_stats = pyloudness.get_loudness("recording.wav")
-        print(loudness_stats)
-        print(loudness_stats['Loudness Range'])
+        return loudness_stats['Loudness Range']['LRA']
+
+def main():
+    score_sum = 0
+    for x in range(1, 5):
+        score_sum += pomodoro()
+        tts.say("Break Time!")
 
 if __name__ == '__main__':
     main()

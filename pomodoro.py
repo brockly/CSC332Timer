86bf44cb79d8419c39e95ff4a0a8c48e3ca30594
Carlos Samaniego <cscarlossamaniego@gmail.com>
2019-11-05 11:44:39 -0500

    created script to handle pomodoro logic

---
+++ pomodoro.py
@@ -0,0 +1,27 @@
+import time
+from aiy.board import Board, Led
+#would have to pass in values from user before the function is called
+def pomodoro(study_session_time, break_time):
+    study_session_time = int(study_session_time)
+    break_time = int(break_time)
+    
+    tts.say("Press Google Home button to start. Press Google Home button to stop")
+    mins = 0
+    
+    #FIXME if statement to say if button on google home pressed, run the pomodoro timer
+    while input != 'stop' or input != 'STOP':   #FIXME modify to button pressed.
+        # Loop until we reach specified session time 
+        while mins != study_session_time:
+            print(">>>>>>>>>>>>>>>>>>>>>", mins)
+            #FIXME HERE: RECORD SOUNDS IN SESSION
+            
+            time.sleep(60)
+            mins += 1
+            
+            #FIXME if statement to say if button pressed again, stop/break
+        
+        time.sleep(break_time)
+        #FIXME SOUND BREAK ALARM!
+        #FIXME Mention amount of high volume sounds made
+        #FIXME RECORD SOUNDS IN SESSION
+        #FIXME if statement to say if button pressed again, stop/break

17fdeff3af4f3f031d92c011e9b39de44ef2c218
Jules Amosah <teomik129@gmail.com>
2019-11-07 01:56:08 -0500

    Aesthetics

--- CSC332Pomodoro.py
+++ CSC332Pomodoro.py
@@ -29,7 +29,6 @@ class Pomodoro:
     def __init__(self, worktime, breaktime):
         self._done = threading.Event()
         self._board = Board()
-        self._board.button.when_pressed = self._done.set
         self._worktime = worktime
         self._breaktime = breaktime
 
@@ -43,7 +42,7 @@ class Pomodoro:
             if duration > interval * 60:
                 break
             print(
-                "{}: {} second(s) left [Press button to stop]".format(
+                "{:8s}: {:07.2f} second(s) left [Press button to stop]".format(
                     state, interval * 60 - duration
                 ),
                 end="\r",
@@ -51,7 +50,9 @@ class Pomodoro:
             )
 
     def _work(self):
-        tts.say("Time to work! I'll rate your productivity based on how quiet you are")
+        tts.say("Time to work!")
+
+        self._board.led.state = Led.PULSE_SLOW
 
         record_file(
             AudioFormat.CD, filename=self._file, wait=self._wait, filetype="wav",
@@ -71,6 +72,8 @@ class Pomodoro:
     def _break(self):
         tts.say("Break time! Relax your brain!")
 
+        self._board.led.state = Led.BEACON_DARK
+
         self._wait("Break")
 
         tts.say("Your break is over!")
@@ -78,24 +81,38 @@ class Pomodoro:
         self._done.clear()
 
     def start(self, reps):
-        self._board.led.state = Led.ON
+        self._board.led.state = Led.BEACON
         tts.say(
             "Welcome to the Pomodoro timer. I am currently set to run {} times. Press the button to start!".format(
                 reps
             )
         )
+        self._board.led.state = Led.PULSE_QUICK
         self._board.button.wait_for_press()
+        self._board.button.when_pressed = self._done.set
+        self._board.led.state = Led.PULSE_SLOW
+        tts.say("I'll rate your productivity based on how quiet you are.")
         for x in range(1, reps):
             tts.say(
                 "The next round begins in 5 seconds, press the button if you would like to exit the timer"
             )
+            self._board.led.state = Led.PULSE_QUICK
             if self._done.wait(5):
                 tts.say("Goodbye!")
+                self._board.led.state = Led.OFF
+                self._board.close()
                 return
+            self._board.led.state = Led.ON
             tts.say("Round {}".format(x))
             self._work()
-            tts.say("Current score: {} noise points".format(self._score))
+            tts.say("Current score: {:03.2f} noise points".format(self._score))
             self._break()
+
+        tts.say(
+            "Congratulations! You have completed the session. Your final score is {:03.2f}".format(
+                self._score
+            )
+        )
 
 
 def main():
